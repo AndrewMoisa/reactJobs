cd7e31ec394ceab68cc3208f6cde27b32ef60d70
AndrewMoisa <113123678+AndrewMoisa@users.noreply.github.com>
2024-10-07 19:12:25 +0200

    Working with ReactForms

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,28 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: "jobs", element: <JobsPage /> },
-      { path: "*", element: <NotFoundPage /> },
-    ],
-  },
-]);
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
+  const addJob = (newJob) => {
+    console.log(newJob);
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "jobs", element: <JobsPage /> },
+        { path: "add-job", element: <AddJobPage addJobSubmit={addJob} /> },
+        { path: "jobs/:id", element: <JobPage />, loader: jobLoader },
+        { path: "*", element: <NotFoundPage /> },
+      ],
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 };
 
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -9,6 +9,7 @@ const JobListing = ({ job }) => {
   if (!showFullDescription) {
     description = description.substring(0, 90) + "...";
   }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -36,7 +37,7 @@ const JobListing = ({ job }) => {
             {job.location}
           </div>
           <Link
-            to={`/job/${job.id}`}
+            to={`/jobs/${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
