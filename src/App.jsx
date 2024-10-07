acc0e24963f7a9d18ce0a3813272589345696f7f
AndrewMoisa <113123678+AndrewMoisa@users.noreply.github.com>
2024-10-07 19:59:18 +0200

    post/delete requests

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,41 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 const App = () => {
-  const addJob = (newJob) => {
-    console.log(newJob);
+  // Add a new job to the database
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  // Delete job from the database
+
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+    return;
+  };
+
+  // Update job in the database
+
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(job),
+    });
+    return;
   };
 
   const router = createBrowserRouter([
@@ -20,7 +51,16 @@ const App = () => {
         { index: true, element: <HomePage /> },
         { path: "jobs", element: <JobsPage /> },
         { path: "add-job", element: <AddJobPage addJobSubmit={addJob} /> },
-        { path: "jobs/:id", element: <JobPage />, loader: jobLoader },
+        {
+          path: "jobs/:id",
+          element: <JobPage deleteJob={deleteJob} />,
+          loader: jobLoader,
+        },
+        {
+          path: "edit-job/:id",
+          element: <EditJobPage updateJobSubmit={updateJob} />,
+          loader: jobLoader,
+        },
         { path: "*", element: <NotFoundPage /> },
       ],
     },
